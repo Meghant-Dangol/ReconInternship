34dc8343ce31577529d2e764a7f71ffb93c9588a
Meghant00 <100550430+Meghant-Dangol@users.noreply.github.com>
2022-02-28 12:31:46 +0545

    Create: Create Login component

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h2 class="text-center">Login Form</h2>
+    <form
+      action=""
+      @submit.prevent.stop="loginHandler"
+      class="form col-md-4 mx-auto"
+    >
+      <div class="form-group">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" v-model="username" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" v-model="password" class="form-control" />
+      </div>
+      <input type="submit" value="Login" class="btn btn-success mt-3" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    loginHandler() {
+      console.log(this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style></style>
